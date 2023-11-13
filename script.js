d7f84e998ce720f62f15f89b2675793b6317bcf5
Paulo Gomes <139072768+PauloGomes1138@users.noreply.github.com>
2023-11-13 09:09:53 -0300

    Resposta ao Exercício

---
+++ script.js
@@ -0,0 +1,20 @@
+const square = (num) => num * num;
+
+// aritmético 
+
+const sayHello = () => console.log("Olá!");
+
+//  comando 
+
+const add = (a, b) => {
+    // Soma dois números e retorna o resultado
+    return a + b;
+  };
+
+  // imprimir resultado sem retorno 
+
+  function sayGoodbye() {
+    console.log("Tchau!");
+  }
+  
+  
